287836f0c10f13ad3c74b84bc9c9e2bcac04ff53
David-Naray <modellingartsyt@gmail.com>
2025-06-28 20:19:34 +0100

    changed functionality so that selection is exclusively on the left click of the mouse, I want to now have right click be to make move commands for selected units though that functionality is yet to be made

--- staticResources/JS_Externals/RaycasterHandling.js
+++ staticResources/JS_Externals/RaycasterHandling.js
@@ -25,100 +25,105 @@ export function intersectsTileMeshes(){
 
 
 export function MouseDownHandling(e) {
-    if (InputState.value == 'neutral') {
-        dragStart = { x: e.clientX, y: e.clientY };
-        isDragging = false;
-        InputState.value = 'BoxClickSelection';
+    if (e.button === 0) {
+        if (InputState.value == 'neutral') {
+            dragStart = { x: e.clientX, y: e.clientY };
+            isDragging = false;
+            InputState.value = 'BoxClickSelection';
+        }
     }
 }
 
 export function MouseMovingHandling(e) {
-    if (InputState.value == 'BoxClickSelection' && DragSelectionKey) {
-        controls.enabled=false
-        
-        const dx = e.clientX - dragStart.x;
-        const dy = e.clientY - dragStart.y;
-
-        if (Math.abs(dx) > 0.3 || Math.abs(dy) > 0.3) {
-            isDragging = true;
-            // draw selection box here
-            console.log("dragging")
+    if (e.button === 0) {
+        if (InputState.value == 'BoxClickSelection' && DragSelectionKey) {
+            controls.enabled=false
             
-        }else{//hide selection box since it would be small
-            isDragging = false; 
-            console.log("area small, removing drag box")
+            const dx = e.clientX - dragStart.x;
+            const dy = e.clientY - dragStart.y;
+
+            if (Math.abs(dx) > 0.3 || Math.abs(dy) > 0.3) {
+                isDragging = true;
+                // draw selection box here
+                console.log("dragging")
+                
+            }else{//hide selection box since it would be small
+                isDragging = false; 
+                console.log("area small, removing drag box")
+            }
         }
     }
 }
 
 export function MouseUpHandling(e) {
-    // console.log("mouse up")
-    if (InputState.value === 'BoxClickSelection') {
-        if (isDragging) {
-            // perform box selection logic
-            console.log("dragging while mouse up?")
-        }else{//just a click effectively
-            console.log("muaahaha, just a click")
-            onPointerMove(e)
-        
-            const intersectsAll = raycaster.intersectObjects(scene.children, true);
-            const intersects = intersectsAll.filter(i => !globalmanager.allTileMeshes.includes(i.object));
+    if (e.button === 0) {
+        if (InputState.value === 'BoxClickSelection') {
+            if (isDragging) {
+                // perform box selection logic
+                console.log("dragging while mouse up?")
+            }else{//just a click effectively
+                console.log("muaahaha, just a click")
+                onPointerMove(e)
+            
+                const intersectsAll = raycaster.intersectObjects(scene.children, true);
+                const intersects = intersectsAll.filter(i => !globalmanager.allTileMeshes.includes(i.object));
 
-            if (intersects.length > 0) {
-                const hit = intersects[0];
+                if (intersects.length > 0) {
+                    const hit = intersects[0];
 
-                if (hit.instanceId !== undefined) {
-                    // console.log('Instanced object hit, instanceId:', hit.instanceId);
-                    // console.log('Base mesh:', hit.object);
-                    
-                    // You can now use hit.instanceId to reference that specific instance
-                    const sendOver=[hit]
-                    UnitSelectionDisplay(sendOver)
-                } else {
-                    console.log('Non-instanced object hit:', hit.object);
+                    if (hit.instanceId !== undefined) {
+                        // console.log('Instanced object hit, instanceId:', hit.instanceId);
+                        // console.log('Base mesh:', hit.object);
+                        
+                        // You can now use hit.instanceId to reference that specific instance
+                        const sendOver=[hit]
+                        UnitSelectionDisplay(sendOver)
+                    } else {
+                        console.log('Non-instanced object hit:', hit.object);
 
-                }
-            }else{
-                var UnitInfoDispContentBox=document.getElementById("UnitInfoDispContentBox");
-                if(UnitInfoDispContentBox && UnitInfoDispContentBox.style.display=="block"){
-                    // UnitInfoDispContentBox.style.display="none"
-                    document.getElementById("Button_Dropdown").style.display="none";
+                    }
+                }else{
+                    var UnitInfoDispContentBox=document.getElementById("UnitInfoDispContentBox");
+                    if(UnitInfoDispContentBox && UnitInfoDispContentBox.style.display=="block"){
+                        // UnitInfoDispContentBox.style.display="none"
+                        document.getElementById("Button_Dropdown").style.display="none";
+                    }
                 }
             }
+            
         }
-        
-    }
-    controls.enabled=true;//back to enabling controls, the default state
-    isDragging = false; //return to false, the neutral state for drag
-    InputState.value = 'neutral';//overall input state
+        controls.enabled=true;//back to enabling controls, the default state
+        isDragging = false; //return to false, the neutral state for drag
+        InputState.value = 'neutral';//overall input state
 
-    //note i could probably combine these two variable but cba.. it works rn
+        //note i could probably combine these two variable but cba.. it works rn
+    }
 }
 
-export function onClickObjectSelection(event) {
-    if (isDragging) {
-        event.stopImmediatePropagation(); // optional: block click if drag happened
-        return;
-    }
+// export function onClickObjectSelection(event) {
+//     if (isDragging) {
+//         event.stopImmediatePropagation(); // optional: block click if drag happened
+//         return;
+//     }
 
 
-    //select everything and then filter out the terrainMeshes
-    if(InputState.value=="neutral"){
-        onPointerMove(event)
+//     //select everything and then filter out the terrainMeshes
+//     if(InputState.value=="neutral"){
+//         onPointerMove(event)
         
-        const intersectsAll = raycaster.intersectObjects(scene.children, true);
-        const intersects = intersectsAll.filter(i => !globalmanager.allTileMeshes.includes(i.object));
+//         const intersectsAll = raycaster.intersectObjects(scene.children, true);
+//         const intersects = intersectsAll.filter(i => !globalmanager.allTileMeshes.includes(i.object));
 
-        if (intersects.length > 0) {
-            const selectedObject = intersects[0].object;
-            console.log('Selected object:', selectedObject);
-            // Do your selection logic here, e.g., highlight, add to selection array, etc.
-        } else {
-            console.log('Nothing selected');
-        } 
-    }
+//         if (intersects.length > 0) {
+//             const selectedObject = intersects[0].object;
+//             console.log('Selected object:', selectedObject);
+//             // Do your selection logic here, e.g., highlight, add to selection array, etc.
+//         } else {
+//             console.log('Nothing selected');
+//         } 
+//     }
 
-}
+// }
 
 //when raycast hitting something as a selection or box selecting, everything is included bar the terrain meshes
 //on the server differentiation occurs and a response is made as to the user akin to {buildings:{..},units:{..}}
